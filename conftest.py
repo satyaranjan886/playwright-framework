b04b46d42adb80582b75ef775e7449aaff0a86d4
satyaranjanswain-droid <satyaranjan.swain@macrovo.com>
2026-06-24 07:15:36 +0530

    add headless

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         yield page
         browser.close()
